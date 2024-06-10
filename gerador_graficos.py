141470a8e2704a6978ab8332174b8e75c1f03be7
allanferre <ferreiraallan19@gmail.com>
2024-06-10 17:27:38 -0300

    gerador de graficos finalizado

--- gerador_graficos.py
+++ gerador_graficos.py
@@ -21,15 +21,8 @@ def gerar_grafico_artistas_mais_citados(arquivo_json):
       if count >= 2:
         artista.append(word)
         citacoes.append(count)
-        print(".....")
-        print(f"{word}: {count}")
-  
-
-  
-#   rotulos = list(contagem_artistas.keys())
-#   valores = list(contagem_artistas.values())
-#   print(".....")
-#   print(f"{artista}: {citacoes}")
+        # print(".....")
+        # print(f"{word}: {count}")
 
   # Gerar gráfico de barras
   plt.figure(figsize=(10, 6))
@@ -48,34 +41,69 @@ def gerar_grafico_duracao_media_por_musica(arquivo_json):
   with open(arquivo_json, 'r') as f:
     dados = json.load(f)
 
- # Extrair dados
-#   nomes_musicas = [musica['nome'] for musica in dados]
-#   duracoes_musicas = [musica['duracao'] for musica in dados]  # Assumindo 'duracao' como valor numérico
-  
+ # Extrair dados e filtrar as musicas que nao consta tempo
   nome = []
   duracao = []
+  nome_sem_tempo = []
+  duracao_sem_tempo = []
   for index, i in dados.items():
+    sem_duracao = i['duracao']
+    if sem_duracao != "Nao consta tempo":
         nome.append(i['nome'])
         duracao.append(i['duracao'])
+    else:
+            nome_sem_tempo.append(i['nome'])
+            duracao_sem_tempo.append(i['duracao']) 
+        
+   
+  # ordenacao pela duracao da musica     
+  dados = list(zip(nome, duracao))
+  dados_ordenados = sorted(dados, key=lambda x: x[1])   
+  nome_ordenado, duracao_ordenada = zip(*dados_ordenados)     
 
-  # Gerar gráfico de barras tipo "dot bar"
+  # Gerar o gráfico
   plt.figure(figsize=(14, 8))
-  plt.bar(nome, duracao, color='skyblue', alpha=0.7)
+  plt.bar(nome_ordenado, duracao_ordenada, color='skyblue')
   plt.xlabel("Nome da Música")
   plt.xticks(rotation=30, ha='right', fontsize=8)
   plt.ylabel("Duração (em minutos)")
-  plt.title("Nome e Duração das Músicas")
+  plt.title("Duração das Músicas")
   plt.tight_layout()
-
-  # Exibir o gráfico no VS Code
   plt.show()
+  
+def gerar_grafico_generos_mais_citados(arquivo_json):
 
-         
-          
+  # Carregar dados do JSON
+  with open(arquivo_json ) as f:
+    dados = json.load(f)
+    
+    generos = []
+    for index, i in dados.items():
+        generos.append(i['genero'])
 
+  contagem_generos = collections.Counter(generos)
+  
+  generos1 = []
+  citacoes = []
+  for word, count in contagem_generos.items():
+      nao_consta_genero = word
+      if count >= 2 and nao_consta_genero !="Nao consta genero":
+        generos1.append(word)
+        citacoes.append(count)
 
+  # Gerar gráfico
+  plt.figure(figsize=(10, 6))
+  plt.bar(generos1, citacoes, color='skyblue')
+  plt.xlabel("Genero")
+  plt.ylabel("Contagem")
+  plt.title("Generos Mais Citados")
+  plt.xticks(rotation=45, ha='right')
+  plt.tight_layout()
+  plt.show()   
 
-# Artistas mais citados
-arquivo_json = 'musicas.json'  # Substitua pelo caminho real do seu arquivo
+# Gerando os graficos
+arquivo_json = 'musicas.json' 
+gerar_grafico_artistas_mais_citados(arquivo_json)
 gerar_grafico_duracao_media_por_musica(arquivo_json)
+gerar_grafico_generos_mais_citados(arquivo_json)
 
